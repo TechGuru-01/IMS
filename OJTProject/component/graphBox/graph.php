d9e11f0375c0dcd68188b2357d508d395d23f6a3
Elijah Boon <elijahboon986@gmail.com>
2026-03-09 15:33:20 +0800

    Delete OJTProject directory

--- OJTProject/component/graphBox/graph.php
+++
@@ -1,6 +0,0 @@
-
-    <div class="box-content box-4" id="box-graph">
-    <div class="inner-box">
-        <h3>Inventory Analytics</h3>
-        </div>
-
--- OJTProject/component/inventoryAlertBox/inventoryAlerts.php
+++
@@ -1,4 +0,0 @@
-<div class="box-content box-3" id="box-history">
-    <div class="inner-box">
-        </div>
-</div>
