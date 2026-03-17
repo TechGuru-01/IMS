c4ffa9b0c40ef7a281c9d7d1446541101c80f6a9
Elijah Boon <elijahboon986@gmail.com>
2026-03-18 02:36:11 +0800

    Delete OJTProject directory

--- OJTProject/component/inventoryAlertBox/inventoryAlerts.js
+++
@@ -1,7 +0,0 @@
-const masterCheck = document.getElementById("selectAllAlerts");
-if (masterCheck) {
-  masterCheck.addEventListener("change", function () {
-    const checks = document.querySelectorAll(".alert-checkbox");
-    checks.forEach((c) => (c.checked = this.checked));
-  });
-}
